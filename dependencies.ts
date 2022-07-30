1b47034afb9de2f9a195313a9b279ab43818046a
Abhinav A V <abhai2k@gmail.com>
2022-07-31 04:21:11 +0530

    Bump version
    Bump version to 0.150 for all std libraries

--- dependencies.ts
+++ dependencies.ts
@@ -1,16 +1,16 @@
 // Begin Core
-export * as path from "https://deno.land/std@0.121.0/path/mod.ts";
-export { ensureDir, ensureFile } from "https://deno.land/std@0.121.0/fs/mod.ts";
-export { format as dateFormat } from "https://deno.land/std@0.121.0/datetime/mod.ts";
+export * as path from "https://deno.land/std@0.150.0/path/mod.ts";
+export { ensureDir, ensureFile } from "https://deno.land/std@0.150.0/fs/mod.ts";
+export { format as dateFormat } from "https://deno.land/std@0.150.0/datetime/mod.ts";
 export {
   BufWriter,
   BufWriterSync,
-} from "https://deno.land/std@0.121.0/io/mod.ts";
-// export * as fs from "https://deno.land/std@0.121.0/fs/mod.ts"
-export { printf, sprintf } from "https://deno.land/std@0.121.0/fmt/printf.ts";
-export * as yaml from "https://deno.land/std@0.121.0/encoding/yaml.ts";
-export * as toml from "https://deno.land/std@0.121.0/encoding/toml.ts";
-export * as base64url from "https://deno.land/std@0.121.0/encoding/base64url.ts";
+} from "https://deno.land/std@0.150.0/io/mod.ts";
+// export * as fs from "https://deno.land/std@0.150.0/fs/mod.ts"
+export { printf, sprintf } from "https://deno.land/std@0.150.0/fmt/printf.ts";
+export * as yaml from "https://deno.land/std@0.150.0/encoding/yaml.ts";
+export * as toml from "https://deno.land/std@0.150.0/encoding/toml.ts";
+export * as base64url from "https://deno.land/std@0.150.0/encoding/base64url.ts";
 export {
   blue,
   bold,
@@ -23,7 +23,7 @@ export {
   red,
   underline,
   yellow,
-} from "https://deno.land/std@0.121.0/fmt/colors.ts";
+} from "https://deno.land/std@0.150.0/fmt/colors.ts";
 
 // export { encode, decode } from "https://deno.land/std@0.121.0/encoding/base64url.ts"
 // End Core
