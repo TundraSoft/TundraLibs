628b7e07d474d3bf382e8f85a38aca0ec3c4b008
Abhinav A V <Abhinav.av@anq.finance>
2023-06-25 20:08:03 +0530

    Update Sysinfo

--- sysinfo/mod.ts
+++ sysinfo/mod.ts
@@ -62,52 +62,54 @@ export const Sysinfo = {
    * @param size HumanSize Human understandable sizes.
    * @returns Promise<MemoryInfo>
    */
-  // Disabling to avoid --unstable
-  // getMemory: async function (size: HumanSizes = "B") {
-  //   const mem: MemoryInfo = {
-  //     total: 0,
-  //     free: 0,
-  //     available: 0,
-  //     swap: {
-  //       total: 0,
-  //       free: 0,
-  //     },
-  //   };
-  //   let sizeCalc = 1;
-  //   switch (size) {
-  //     case "TB":
-  //       sizeCalc = 1024 * 1024 * 1024;
-  //       break;
-  //     case "GB":
-  //       sizeCalc = 1024 * 1024;
-  //       break;
-  //     case "MB":
-  //       sizeCalc = 1024;
-  //       break;
-  //     case "KB":
-  //       sizeCalc = 1;
-  //       break;
-  //     default:
-  //     case "B":
-  //       sizeCalc = 1 / 1024;
-  //       break;
-  //   }
-  //   try {
-  //     const checkEnv = await Deno.permissions.query({ name: "env" });
-  //     if (checkEnv.state === "granted") {
-  //       const memInfo = Deno.systemMemoryInfo();
-  //       mem.available = memInfo.available / sizeCalc;
-  //       mem.free = memInfo.free / sizeCalc;
-  //       mem.total = memInfo.total / sizeCalc;
-  //       mem.swap.total = memInfo.swapTotal / sizeCalc;
-  //       mem.swap.free = memInfo.swapFree / sizeCalc;
-  //     }
-  //   } catch (e) {
-  //     // Supress error
-  //     console.log(e);
-  //   }
-  //   return (mem.total === 0) ? undefined : mem;
-  // },
+  getMemory: async function (size: HumanSizes = 'B') {
+    const mem: MemoryInfo = {
+      total: 0,
+      free: 0,
+      available: 0,
+      swap: {
+        total: 0,
+        free: 0,
+      },
+    };
+    let sizeCalc = 1;
+    switch (size) {
+      case 'TB':
+        sizeCalc = 1024 * 1024 * 1024;
+        break;
+      case 'GB':
+        sizeCalc = 1024 * 1024;
+        break;
+      case 'MB':
+        sizeCalc = 1024;
+        break;
+      case 'KB':
+        sizeCalc = 1;
+        break;
+      default:
+      case 'B':
+        sizeCalc = 1 / 1024;
+        break;
+    }
+    try {
+      const checkEnv = await Deno.permissions.query({
+        name: 'sys',
+        kind: 'systemMemoryInfo',
+      });
+      if (checkEnv.state === 'granted') {
+        const memInfo = Deno.systemMemoryInfo();
+        mem.available = memInfo.available / sizeCalc;
+        mem.free = memInfo.free / sizeCalc;
+        mem.total = memInfo.total / sizeCalc;
+        mem.swap.total = memInfo.swapTotal / sizeCalc;
+        mem.swap.free = memInfo.swapFree / sizeCalc;
+      }
+    } catch (e) {
+      // Supress error
+      console.log(e);
+    }
+    return (mem.total === 0) ? undefined : mem;
+  },
 
   /**
    * getLoad
@@ -117,19 +119,21 @@ export const Sysinfo = {
    *
    * @returns Promise<Array<number> | undefined> return 1, 5 and 15 minute load average
    */
-  // disabling to avoud --unstable
-  // getLoad: async function (): Promise<Array<number> | undefined> {
-  //   try {
-  //     const checkEnv = await Deno.permissions.query({ name: "env" });
-  //     if (checkEnv.state === "granted") {
-  //       return await Deno.loadavg();
-  //     }
-  //   } catch (e) {
-  //     // supress error
-  //     console.log(e);
-  //   }
-  //   return undefined;
-  // },
+  getLoad: async function (): Promise<Array<number> | undefined> {
+    try {
+      const checkEnv = await Deno.permissions.query({
+        name: 'sys',
+        kind: 'loadavg',
+      });
+      if (checkEnv.state === 'granted') {
+        return await Deno.loadavg();
+      }
+    } catch (e) {
+      // supress error
+      console.log(e);
+    }
+    return undefined;
+  },
 
   /**
    * getPid
@@ -160,24 +164,18 @@ export const Sysinfo = {
    * @returns Promise<string | undefined> The IP address assigned to the system
    */
   getIP: function (onlyPublic = false): string[] {
-    const ipAddress: string[] = [],
-      networks = Deno.networkInterfaces();
-    for (const network of networks) {
-      if (onlyPublic === true) {
-        if (network.family === 'IPv4') {
+    const networks = Deno.networkInterfaces();
+    const ipAddress = Object.values(networks)
+      .flat()
+      .filter((network) => {
+        if (onlyPublic === true && network.family === 'IPv4') {
           const regex =
             /(^192\.168\.([0-9]|[0-9][0-9]|[0-2][0-5][0-5])\.([0-9]|[0-9][0-9]|[0-2][0-5][0-5])$)|(^172\.([1][6-9]|[2][0-9]|[3][0-1])\.([0-9]|[0-9][0-9]|[0-2][0-5][0-5])\.([0-9]|[0-9][0-9]|[0-2][0-5][0-5])$)|(^10\.([0-9]|[0-9][0-9]|[0-2][0-5][0-5])\.([0-9]|[0-9][0-9]|[0-2][0-5][0-5])\.([0-9]|[0-9][0-9]|[0-2][0-5][0-5])$)/;
-          if (regex.test(network.address)) {
-            ipAddress.push(network.address);
-          }
-        } else if (network.family === 'IPv6') {
-          // @TODO - Add check here
-          ipAddress.push(network.address);
+          return regex.test(network.address);
         }
-      } else {
-        ipAddress.push(network.address);
-      }
-    }
+        return true;
+      })
+      .map((network) => network.address);
     return ipAddress;
   },
 
@@ -265,3 +263,5 @@ export const Sysinfo = {
     return __envData.has(name);
   },
 };
+
+console.log(Sysinfo.getIP());
