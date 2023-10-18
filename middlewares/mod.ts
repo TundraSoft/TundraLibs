00d9ea279645e5c84ccd9e41744cd25fdb157bad
rht16 <rohitkrrt@gmail.com>
2023-10-18 19:24:59 +0530

    mod file added for escape middleware

--- middlewares/mod.ts
+++ middlewares/mod.ts
@@ -2,3 +2,5 @@ export { timing } from './timing/mod.ts';
 export { requestId } from './requestId/mod.ts';
 export { throttle } from './throttle/mod.ts';
 export type { ThrottleOptions } from './throttle/mod.ts';
+
+export { Escape } from './escape/mod.ts'
