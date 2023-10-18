0d025548f89aa19e2b277dc16f578793163aab42
rht16 <rohitkrrt@gmail.com>
2023-10-18 15:25:11 +0530

    cors config middleware added

--- middlewares/mod.ts
+++ middlewares/mod.ts
@@ -2,3 +2,4 @@ export { timing } from './timing/mod.ts';
 export { requestId } from './requestId/mod.ts';
 export { throttle } from './throttle/mod.ts';
 export type { ThrottleOptions } from './throttle/mod.ts';
+export { oakCors } from './cors/mod'
