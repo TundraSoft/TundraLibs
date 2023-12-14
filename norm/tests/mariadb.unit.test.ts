bcf9770cbb7cf7db2a965d8efd9c58ec435b3cb2
consuitant <consuitant@anq.finance>
2023-12-14 06:10:42 +0000

    MariaDB tests initial commit - they fail because of no impl

---
+++ norm/tests/mariadb.unit.test.ts
@@ -0,0 +1,20 @@
+import { describe } from '../../dev.dependencies.ts';
+import { MariaClient } from '../Dialects/mod.ts';
+import type { MariaConnectionOptions } from '../types/mod.ts';
+import { 
+  makeMariaOptions,
+  runStandardConnectionTests,
+  runStandardQueryTests
+} from './testdata/utils/ClientTestHelper.ts';
+
+describe(`[library='norm' dialect='MARIADB' type='unit']`, () => {
+  const clientOpt: MariaConnectionOptions = makeMariaOptions();
+  runStandardConnectionTests(
+    MariaClient,
+    clientOpt
+  );
+  runStandardQueryTests(    
+    MariaClient,
+    clientOpt
+  );
+});
