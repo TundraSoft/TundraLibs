12cdbcd0923524e6c23bd93bfe43f6720bbefad5
Abhinav A V <36784+abhai2k@users.noreply.github.com>
2024-01-12 02:36:19 +0530

    Format & Pipeline

--- restler/RESTler.ts
+++ restler/RESTler.ts
@@ -149,7 +149,8 @@ export abstract class RESTler<
           signal: controller.signal,
           body: (request.body === undefined)
             ? undefined
-            : (request.body instanceof FormData || typeof request.body === 'string')
+            : (request.body instanceof FormData ||
+                typeof request.body === 'string')
             ? request.body
             : this._stringifyBody(request.body),
         },
