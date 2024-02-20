6d83cbfe8494a6db63d8c86122a96374088d4396
Abhinav A V <36784+abhai2k@users.noreply.github.com>
2024-02-20 10:31:12 +0530

    Add testing for debounce

---
+++ utils/decorators/debounce.test.ts
@@ -0,0 +1,34 @@
+import { debounce } from './debounce.ts';
+import { assertEquals, describe, it } from '../../dev.dependencies.ts';
+
+describe({
+  name: 'utils',
+  sanitizeExit: false,
+  sanitizeOps: false,
+  sanitizeResources: false,
+}, () => {
+  describe('decorators', () => {
+    describe('throttle', () => {
+      class Test {
+        public cnt = 0;
+      
+        @debounce(1000)
+        public methodName() {
+          this.cnt += 1;
+        }
+      }
+
+      const a = new Test();
+
+      it('should throttle the method execution', async() => {
+        assertEquals(a.cnt, 0);
+        a.methodName();
+        a.methodName();
+        a.methodName();
+        a.methodName();
+        await new Promise((resolve) => setTimeout(resolve, 1200));
+        assertEquals(a.cnt, 1);
+      });
+    });
+  });
+});
