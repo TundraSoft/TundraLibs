a2e1f1a7ac097f38914198fae2bb067b8828238a
Abhinav A V <36784+abhai2k@users.noreply.github.com>
2024-03-29 03:29:24 +0530

    Fix formatting

--- utils/encrypt.test.ts
+++ utils/encrypt.test.ts
@@ -4,10 +4,9 @@ import {
   describe,
   it,
 } from '../dev.dependencies.ts';
-import { encrypt, decrypt } from './encrypt.ts';
+import { decrypt, encrypt } from './encrypt.ts';
 
 describe('utils', () => {
-  
   describe({ name: 'encrypt' }, () => {
     const key = '1111111111111111';
     const data = { a: 1, b: 2, c: 3 };
@@ -23,7 +22,7 @@ describe('utils', () => {
           () =>
             encrypt(
               'data',
-              key, 
+              key,
               JSON.parse(
                 JSON.stringify({ algorithm: 'invalid', encoding: 'HEX' }),
               ),
@@ -35,8 +34,8 @@ describe('utils', () => {
         await assertRejects(
           () =>
             decrypt(
-              'data', 
-              key, 
+              'data',
+              key,
               JSON.parse(
                 JSON.stringify({ algorithm: 'invalid', encoding: 'HEX' }),
               ),
@@ -49,9 +48,9 @@ describe('utils', () => {
       it('invalid encoding', async () => {
         await assertRejects(
           () =>
-          encrypt(
+            encrypt(
               'data',
-              key, 
+              key,
               JSON.parse(
                 JSON.stringify({ algorithm: 'AES-CBC', encoding: 'invalid' }),
               ),
@@ -62,9 +61,9 @@ describe('utils', () => {
 
         await assertRejects(
           () =>
-          decrypt(
+            decrypt(
               'data',
-              key, 
+              key,
               JSON.parse(
                 JSON.stringify({ algorithm: 'AES-CBC', encoding: 'invalid' }),
               ),
@@ -73,134 +72,276 @@ describe('utils', () => {
           'Invalid encoding',
         );
       });
-      
     });
 
     describe('AES-CBC', () => {
       it('should encrypt and decrypt data', async () => {
-        const encrypted = await encrypt(data, key, { algorithm: 'AES-CBC', encoding: 'HEX' });
-        const decrypted = await decrypt(encrypted, key, { algorithm: 'AES-CBC', encoding: 'HEX' });
+        const encrypted = await encrypt(data, key, {
+          algorithm: 'AES-CBC',
+          encoding: 'HEX',
+        });
+        const decrypted = await decrypt(encrypted, key, {
+          algorithm: 'AES-CBC',
+          encoding: 'HEX',
+        });
         assertEquals(decrypted, data);
 
-        const encrypted2 = await encrypt(data, key, { algorithm: 'AES-CBC', encoding: 'BASE64' });
-        const decrypted2 = await decrypt(encrypted2, key, { algorithm: 'AES-CBC', encoding: 'BASE64' });
+        const encrypted2 = await encrypt(data, key, {
+          algorithm: 'AES-CBC',
+          encoding: 'BASE64',
+        });
+        const decrypted2 = await decrypt(encrypted2, key, {
+          algorithm: 'AES-CBC',
+          encoding: 'BASE64',
+        });
         assertEquals(decrypted2, data);
       });
 
       it('should encrypt and decrypt string', async () => {
-        const encrypted = await encrypt(str, key, { algorithm: 'AES-CBC', encoding: 'HEX' });
-        const decrypted = await decrypt(encrypted, key, { algorithm: 'AES-CBC', encoding: 'HEX' });
+        const encrypted = await encrypt(str, key, {
+          algorithm: 'AES-CBC',
+          encoding: 'HEX',
+        });
+        const decrypted = await decrypt(encrypted, key, {
+          algorithm: 'AES-CBC',
+          encoding: 'HEX',
+        });
         assertEquals(decrypted, str);
 
-        const encrypted2 = await encrypt(str, key, { algorithm: 'AES-CBC', encoding: 'BASE64' });
-        const decrypted2 = await decrypt(encrypted2, key, { algorithm: 'AES-CBC', encoding: 'BASE64' });
+        const encrypted2 = await encrypt(str, key, {
+          algorithm: 'AES-CBC',
+          encoding: 'BASE64',
+        });
+        const decrypted2 = await decrypt(encrypted2, key, {
+          algorithm: 'AES-CBC',
+          encoding: 'BASE64',
+        });
         assertEquals(decrypted2, str);
       });
 
       it('should encrypt and decrypt number', async () => {
-        const encrypted = await encrypt(num, key, { algorithm: 'AES-CBC', encoding: 'HEX' });
-        const decrypted = await decrypt(encrypted, key, { algorithm: 'AES-CBC', encoding: 'HEX' });
+        const encrypted = await encrypt(num, key, {
+          algorithm: 'AES-CBC',
+          encoding: 'HEX',
+        });
+        const decrypted = await decrypt(encrypted, key, {
+          algorithm: 'AES-CBC',
+          encoding: 'HEX',
+        });
         assertEquals(decrypted, num);
 
-        const encrypted2 = await encrypt(num, key, { algorithm: 'AES-CBC', encoding: 'BASE64' });
-        const decrypted2 = await decrypt(encrypted2, key, { algorithm: 'AES-CBC', encoding: 'BASE64' });
+        const encrypted2 = await encrypt(num, key, {
+          algorithm: 'AES-CBC',
+          encoding: 'BASE64',
+        });
+        const decrypted2 = await decrypt(encrypted2, key, {
+          algorithm: 'AES-CBC',
+          encoding: 'BASE64',
+        });
         assertEquals(decrypted2, num);
       });
 
       it('should encrypt and decrypt array', async () => {
-        const encrypted = await encrypt(arr, key, { algorithm: 'AES-CBC', encoding: 'HEX' });
-        const decrypted = await decrypt(encrypted, key, { algorithm: 'AES-CBC', encoding: 'HEX' });
+        const encrypted = await encrypt(arr, key, {
+          algorithm: 'AES-CBC',
+          encoding: 'HEX',
+        });
+        const decrypted = await decrypt(encrypted, key, {
+          algorithm: 'AES-CBC',
+          encoding: 'HEX',
+        });
         assertEquals(decrypted, arr);
 
-        const encrypted2 = await encrypt(arr, key, { algorithm: 'AES-CBC', encoding: 'BASE64' });
-        const decrypted2 = await decrypt(encrypted2, key, { algorithm: 'AES-CBC', encoding: 'BASE64' });
+        const encrypted2 = await encrypt(arr, key, {
+          algorithm: 'AES-CBC',
+          encoding: 'BASE64',
+        });
+        const decrypted2 = await decrypt(encrypted2, key, {
+          algorithm: 'AES-CBC',
+          encoding: 'BASE64',
+        });
         assertEquals(decrypted2, arr);
       });
     });
 
     describe('AES-CTR', () => {
       it('should encrypt and decrypt data', async () => {
-        const encrypted = await encrypt(data, key, { algorithm: 'AES-CTR', encoding: 'HEX' });
-        const decrypted = await decrypt(encrypted, key, { algorithm: 'AES-CTR', encoding: 'HEX' });
+        const encrypted = await encrypt(data, key, {
+          algorithm: 'AES-CTR',
+          encoding: 'HEX',
+        });
+        const decrypted = await decrypt(encrypted, key, {
+          algorithm: 'AES-CTR',
+          encoding: 'HEX',
+        });
         assertEquals(decrypted, data);
 
-        const encrypted2 = await encrypt(data, key, { algorithm: 'AES-CTR', encoding: 'BASE64' });
-        const decrypted2 = await decrypt(encrypted2, key, { algorithm: 'AES-CTR', encoding: 'BASE64' });
+        const encrypted2 = await encrypt(data, key, {
+          algorithm: 'AES-CTR',
+          encoding: 'BASE64',
+        });
+        const decrypted2 = await decrypt(encrypted2, key, {
+          algorithm: 'AES-CTR',
+          encoding: 'BASE64',
+        });
         assertEquals(decrypted2, data);
       });
 
       it('should encrypt and decrypt string', async () => {
-        const encrypted = await encrypt(str, key, { algorithm: 'AES-CTR', encoding: 'HEX' });
-        const decrypted = await decrypt(encrypted, key, { algorithm: 'AES-CTR', encoding: 'HEX' });
+        const encrypted = await encrypt(str, key, {
+          algorithm: 'AES-CTR',
+          encoding: 'HEX',
+        });
+        const decrypted = await decrypt(encrypted, key, {
+          algorithm: 'AES-CTR',
+          encoding: 'HEX',
+        });
         assertEquals(decrypted, str);
 
-        const encrypted2 = await encrypt(str, key, { algorithm: 'AES-CTR', encoding: 'BASE64' });
-        const decrypted2 = await decrypt(encrypted2, key, { algorithm: 'AES-CTR', encoding: 'BASE64' });
+        const encrypted2 = await encrypt(str, key, {
+          algorithm: 'AES-CTR',
+          encoding: 'BASE64',
+        });
+        const decrypted2 = await decrypt(encrypted2, key, {
+          algorithm: 'AES-CTR',
+          encoding: 'BASE64',
+        });
         assertEquals(decrypted2, str);
       });
 
       it('should encrypt and decrypt number', async () => {
-        const encrypted = await encrypt(num, key, { algorithm: 'AES-CTR', encoding: 'HEX' });
-        const decrypted = await decrypt(encrypted, key, { algorithm: 'AES-CTR', encoding: 'HEX' });
+        const encrypted = await encrypt(num, key, {
+          algorithm: 'AES-CTR',
+          encoding: 'HEX',
+        });
+        const decrypted = await decrypt(encrypted, key, {
+          algorithm: 'AES-CTR',
+          encoding: 'HEX',
+        });
         assertEquals(decrypted, num);
 
-        const encrypted2 = await encrypt(num, key, { algorithm: 'AES-CTR', encoding: 'BASE64' });
-        const decrypted2 = await decrypt(encrypted2, key, { algorithm: 'AES-CTR', encoding: 'BASE64' });
+        const encrypted2 = await encrypt(num, key, {
+          algorithm: 'AES-CTR',
+          encoding: 'BASE64',
+        });
+        const decrypted2 = await decrypt(encrypted2, key, {
+          algorithm: 'AES-CTR',
+          encoding: 'BASE64',
+        });
         assertEquals(decrypted2, num);
       });
 
       it('should encrypt and decrypt array', async () => {
-        const encrypted = await encrypt(arr, key, { algorithm: 'AES-CTR', encoding: 'HEX' });
-        const decrypted = await decrypt(encrypted, key, { algorithm: 'AES-CTR', encoding: 'HEX' });
+        const encrypted = await encrypt(arr, key, {
+          algorithm: 'AES-CTR',
+          encoding: 'HEX',
+        });
+        const decrypted = await decrypt(encrypted, key, {
+          algorithm: 'AES-CTR',
+          encoding: 'HEX',
+        });
         assertEquals(decrypted, arr);
 
-        const encrypted2 = await encrypt(arr, key, { algorithm: 'AES-CTR', encoding: 'BASE64' });
-        const decrypted2 = await decrypt(encrypted2, key, { algorithm: 'AES-CTR', encoding: 'BASE64' });
+        const encrypted2 = await encrypt(arr, key, {
+          algorithm: 'AES-CTR',
+          encoding: 'BASE64',
+        });
+        const decrypted2 = await decrypt(encrypted2, key, {
+          algorithm: 'AES-CTR',
+          encoding: 'BASE64',
+        });
         assertEquals(decrypted2, arr);
       });
     });
 
     describe('AES-GCM', () => {
       it('should encrypt and decrypt data', async () => {
-        const encrypted = await encrypt(data, key, { algorithm: 'AES-GCM', encoding: 'HEX' });
-        const decrypted = await decrypt(encrypted, key, { algorithm: 'AES-GCM', encoding: 'HEX' });
+        const encrypted = await encrypt(data, key, {
+          algorithm: 'AES-GCM',
+          encoding: 'HEX',
+        });
+        const decrypted = await decrypt(encrypted, key, {
+          algorithm: 'AES-GCM',
+          encoding: 'HEX',
+        });
         assertEquals(decrypted, data);
 
-        const encrypted2 = await encrypt(data, key, { algorithm: 'AES-GCM', encoding: 'BASE64' });
-        const decrypted2 = await decrypt(encrypted2, key, { algorithm: 'AES-GCM', encoding: 'BASE64' });
+        const encrypted2 = await encrypt(data, key, {
+          algorithm: 'AES-GCM',
+          encoding: 'BASE64',
+        });
+        const decrypted2 = await decrypt(encrypted2, key, {
+          algorithm: 'AES-GCM',
+          encoding: 'BASE64',
+        });
         assertEquals(decrypted2, data);
       });
 
       it('should encrypt and decrypt string', async () => {
-        const encrypted = await encrypt(str, key, { algorithm: 'AES-GCM', encoding: 'HEX' });
-        const decrypted = await decrypt(encrypted, key, { algorithm: 'AES-GCM', encoding: 'HEX' });
+        const encrypted = await encrypt(str, key, {
+          algorithm: 'AES-GCM',
+          encoding: 'HEX',
+        });
+        const decrypted = await decrypt(encrypted, key, {
+          algorithm: 'AES-GCM',
+          encoding: 'HEX',
+        });
         assertEquals(decrypted, str);
 
-        const encrypted2 = await encrypt(str, key, { algorithm: 'AES-GCM', encoding: 'BASE64' });
-        const decrypted2 = await decrypt(encrypted2, key, { algorithm: 'AES-GCM', encoding: 'BASE64' });
+        const encrypted2 = await encrypt(str, key, {
+          algorithm: 'AES-GCM',
+          encoding: 'BASE64',
+        });
+        const decrypted2 = await decrypt(encrypted2, key, {
+          algorithm: 'AES-GCM',
+          encoding: 'BASE64',
+        });
         assertEquals(decrypted2, str);
       });
 
       it('should encrypt and decrypt number', async () => {
-        const encrypted = await encrypt(num, key, { algorithm: 'AES-GCM', encoding: 'HEX' });
-        const decrypted = await decrypt(encrypted, key, { algorithm: 'AES-GCM', encoding: 'HEX' });
+        const encrypted = await encrypt(num, key, {
+          algorithm: 'AES-GCM',
+          encoding: 'HEX',
+        });
+        const decrypted = await decrypt(encrypted, key, {
+          algorithm: 'AES-GCM',
+          encoding: 'HEX',
+        });
         assertEquals(decrypted, num);
 
-        const encrypted2 = await encrypt(num, key, { algorithm: 'AES-GCM', encoding: 'BASE64' });
-        const decrypted2 = await decrypt(encrypted2, key, { algorithm: 'AES-GCM', encoding: 'BASE64' });
+        const encrypted2 = await encrypt(num, key, {
+          algorithm: 'AES-GCM',
+          encoding: 'BASE64',
+        });
+        const decrypted2 = await decrypt(encrypted2, key, {
+          algorithm: 'AES-GCM',
+          encoding: 'BASE64',
+        });
         assertEquals(decrypted2, num);
       });
 
       it('should encrypt and decrypt array', async () => {
-        const encrypted = await encrypt(arr, key, { algorithm: 'AES-GCM', encoding: 'HEX' });
-        const decrypted = await decrypt(encrypted, key, { algorithm: 'AES-GCM', encoding: 'HEX' });
+        const encrypted = await encrypt(arr, key, {
+          algorithm: 'AES-GCM',
+          encoding: 'HEX',
+        });
+        const decrypted = await decrypt(encrypted, key, {
+          algorithm: 'AES-GCM',
+          encoding: 'HEX',
+        });
         assertEquals(decrypted, arr);
 
-        const encrypted2 = await encrypt(arr, key, { algorithm: 'AES-GCM', encoding: 'BASE64' });
-        const decrypted2 = await decrypt(encrypted2, key, { algorithm: 'AES-GCM', encoding: 'BASE64' });
+        const encrypted2 = await encrypt(arr, key, {
+          algorithm: 'AES-GCM',
+          encoding: 'BASE64',
+        });
+        const decrypted2 = await decrypt(encrypted2, key, {
+          algorithm: 'AES-GCM',
+          encoding: 'BASE64',
+        });
         assertEquals(decrypted2, arr);
       });
     });
-
   });
 });
--- utils/encrypt.ts
+++ utils/encrypt.ts
@@ -6,19 +6,19 @@ type EncryptOptions = {
 };
 
 const defaultOptions: EncryptOptions = {
-  algorithm: 'AES-CBC', 
-  encoding: 'HEX', 
-}
+  algorithm: 'AES-CBC',
+  encoding: 'HEX',
+};
 
-const enc = new TextEncoder(), 
+const enc = new TextEncoder(),
   dec = new TextDecoder();
 
 export const encrypt = async (
   message: unknown,
   key: string,
-  opt?: Partial<EncryptOptions>
+  opt?: Partial<EncryptOptions>,
 ): Promise<string> => {
-  const encOpt = {...defaultOptions, ...opt };
+  const encOpt = { ...defaultOptions, ...opt };
   if (
     !encOpt.algorithm ||
     !['AES-CTR', 'AES-CBC', 'AES-GCM'].includes(encOpt.algorithm)
@@ -28,10 +28,15 @@ export const encrypt = async (
   if (!encOpt.encoding || !['HEX', 'BASE64'].includes(encOpt.encoding)) {
     throw new Error('Invalid encoding');
   }
-  const encopt: { name: 'AES-CTR' | 'AES-CBC' | 'AES-GCM', counter?: Uint8Array, length?: number, iv?: Uint8Array } = {
+  const encopt: {
+    name: 'AES-CTR' | 'AES-CBC' | 'AES-GCM';
+    counter?: Uint8Array;
+    length?: number;
+    iv?: Uint8Array;
+  } = {
     name: encOpt.algorithm,
   };
-  if(encOpt.algorithm === 'AES-CTR') {
+  if (encOpt.algorithm === 'AES-CTR') {
     encopt.counter = crypto.getRandomValues(new Uint8Array(16));
     encopt.length = 64;
   } else {
@@ -44,20 +49,28 @@ export const encrypt = async (
       false,
       ['encrypt'],
     ),
-    encrypted = await crypto.subtle.encrypt(encopt, cryptKey, enc.encode(JSON.stringify(message)));
-  if(opt?.encoding === 'HEX') {
-    return `${hex.encodeHex(encrypted)}:${hex.encodeHex(encopt.iv || encopt.counter || '')}`;
+    encrypted = await crypto.subtle.encrypt(
+      encopt,
+      cryptKey,
+      enc.encode(JSON.stringify(message)),
+    );
+  if (opt?.encoding === 'HEX') {
+    return `${hex.encodeHex(encrypted)}:${
+      hex.encodeHex(encopt.iv || encopt.counter || '')
+    }`;
   } else {
-    return `${base64.encodeBase64(encrypted)}:${base64.encodeBase64(encopt.iv || encopt.counter || '')}`;
+    return `${base64.encodeBase64(encrypted)}:${
+      base64.encodeBase64(encopt.iv || encopt.counter || '')
+    }`;
   }
 };
 
 export const decrypt = async <T extends unknown = unknown>(
   message: string,
   key: string,
-  opt?: Partial<EncryptOptions>
+  opt?: Partial<EncryptOptions>,
 ): Promise<T> => {
-  const encOpt = {...defaultOptions, ...opt };
+  const encOpt = { ...defaultOptions, ...opt };
   if (
     !encOpt.algorithm ||
     !['AES-CTR', 'AES-CBC', 'AES-GCM'].includes(encOpt.algorithm)
@@ -68,11 +81,18 @@ export const decrypt = async <T extends unknown = unknown>(
     throw new Error('Invalid encoding');
   }
   const spl = message.split(':'),
-    iv = encOpt.encoding === 'HEX' ? hex.decodeHex(spl[1]) : base64.decodeBase64(spl[1]), 
-    encopt: { name: 'AES-CTR' | 'AES-CBC' | 'AES-GCM', counter?: Uint8Array, length?: number, iv?: Uint8Array } = {
-    name: encOpt.algorithm,
-  };
-  if(encOpt.algorithm === 'AES-CTR') {
+    iv = encOpt.encoding === 'HEX'
+      ? hex.decodeHex(spl[1])
+      : base64.decodeBase64(spl[1]),
+    encopt: {
+      name: 'AES-CTR' | 'AES-CBC' | 'AES-GCM';
+      counter?: Uint8Array;
+      length?: number;
+      iv?: Uint8Array;
+    } = {
+      name: encOpt.algorithm,
+    };
+  if (encOpt.algorithm === 'AES-CTR') {
     encopt.counter = iv;
     encopt.length = 64;
   } else {
@@ -85,11 +105,13 @@ export const decrypt = async <T extends unknown = unknown>(
       false,
       ['decrypt'],
     ),
-    encrypted = encOpt.encoding === 'HEX' ? hex.decodeHex(spl[0]) : base64.decodeBase64(spl[0]),
+    encrypted = encOpt.encoding === 'HEX'
+      ? hex.decodeHex(spl[0])
+      : base64.decodeBase64(spl[0]),
     decrypted = await crypto.subtle.decrypt(
       encopt,
       cryptKey,
       encrypted,
     );
-  return JSON.parse(dec.decode(decrypted));  
+  return JSON.parse(dec.decode(decrypted));
 };
