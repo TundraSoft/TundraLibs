64cfd2f546ad8f5d4e4c8f7b5eecf0172b320689
Abhinav A V <abhinav.av@anq.finance>
2024-06-23 12:29:48 +0530

    Add long query support

--- norm/DatabaseManager.ts
+++ norm/DatabaseManager.ts
@@ -18,7 +18,11 @@ export class DatabaseManager {
   private static _configs: Map<string, ClientConfig> = new Map();
   private static _instance: Map<string, AbstractClient> = new Map();
   public static onPoolWait: (name: string, size: number) => void = () => {};
-  public static onLongQuery: (name: string, query: unknown, time: number) => void = () => {};
+  public static onLongQuery: (
+    name: string,
+    query: unknown,
+    time: number,
+  ) => void = () => {};
 
   static register<T extends ClientConfig>(name: string, config: T) {
     const nameClean = name.trim().toLowerCase();
