b03ed456c0dc6f32236c8f85205eac1dc1df023a
Abhinav A V <36784+abhai2k@users.noreply.github.com>
2026-04-02 17:00:40 +0530

    Fix nullable issue

--- guardian/helpers/mod.ts
+++ guardian/helpers/mod.ts
@@ -4,5 +4,6 @@ export { isIn } from "./isIn.ts";
 export { isNotIn } from "./isNotIn.ts";
 export { isPromiseLike } from "./isPromiseLike.ts";
 export { notEquals } from "./notEquals.ts";
+export { nullable } from "./nullable.ts";
 export { optional } from "./optional.ts";
 export { test } from "./test.ts";
